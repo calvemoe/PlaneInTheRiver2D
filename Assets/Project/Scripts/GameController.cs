19ca0929d9fcef771a2b2d9a86b07bff710faf2e
calvemoe <thephonep@gmail.com>
2019-05-29 07:16:21 +0300

    refactoring optimized ui updating

--- Assets/Project/Scripts/GameController.cs
+++ Assets/Project/Scripts/GameController.cs
@@ -7,18 +7,28 @@ using ZenvaVR;
 
 public class GameController : MonoBehaviour {
 
-    public Text scoteText;
-    public Text fuelText;
-    public Player player;
-    public GameObject mainCamera;
+    [SerializeField]
+    private Text scoteText;
+    [SerializeField]
+    private Text fuelText;
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private GameObject mainCamera;
 
-    public ObjectPool enemyPool;
-    public float enemySpawnInterval = 1f;
-    public float horizontalLimit = 2.8f;
+    [SerializeField]
+    private ObjectPool enemyPool;
+    [SerializeField]
+    private float enemySpawnInterval = 1f;
+    [SerializeField]
+    private float horizontalLimit = 2.8f;
 
-    public GameObject fuelPrefab;
-    public float fuelSpawnInterval = 9f;
-    public float fuelDecreaseSpeed = 5f;
+    [SerializeField]
+    private GameObject fuelPrefab;
+    [SerializeField]
+    private float fuelSpawnInterval = 9f;
+    [SerializeField]
+    private float fuelDecreaseSpeed = 5f;
 
     private float enemySpawnTimer;
     private int score = 0;
@@ -26,89 +36,84 @@ public class GameController : MonoBehaviour {
     private float fuelSpawnTimer;
     private float restartTimer = 3f;
 
-	// Use this for initialization
+    Transform mainCameraTransform;
+    Transform playerTransform;
+
+    void Awake() {
+        mainCameraTransform = mainCamera.transform;
+        playerTransform = player.transform;
+    }
+
 	void Start () {
         enemySpawnTimer = enemySpawnInterval;
         fuelSpawnTimer = Random.Range(0f, fuelSpawnInterval);
 
         player.OnCollect += OnCollectFuel;
+        scoteText.text = score.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
         //enemy spawning
-        if (player != null)
-        {
+        if (player != null) {
             enemySpawnTimer -= Time.deltaTime;
-            if (enemySpawnTimer <= 0)
-            {
+            if (enemySpawnTimer <= 0) {
                 enemySpawnTimer = enemySpawnInterval;
                 GameObject enemyInstance = enemyPool.GetObj();
 
                 enemyInstance.transform.SetParent(transform);
                 enemyInstance.transform.position = new Vector2(
                     Random.Range(-horizontalLimit, horizontalLimit),
-                    player.transform.position.y + Screen.height / 100f);
+                    playerTransform.position.y + Screen.height / 100f);
 
                 if(!enemyInstance.GetComponent<Enemy>().HasOnKill())
                     enemyInstance.GetComponent<Enemy>().OnKill += OnEnemyKill;
-
             }
 
             fuelSpawnTimer -= Time.deltaTime;
-            if (fuelSpawnTimer <= 0)
-            {
+            if (fuelSpawnTimer <= 0) {
                 fuelSpawnTimer = fuelSpawnInterval;
                 GameObject fuelInstance = Instantiate(fuelPrefab);
 
                 fuelInstance.transform.SetParent(transform);
                 fuelInstance.transform.position = new Vector2(
                     Random.Range(-horizontalLimit, horizontalLimit),
-                    player.transform.position.y + Screen.height / 100f);
+                    playerTransform.position.y + Screen.height / 100f);
             }
 
             fuel -= Time.deltaTime * fuelDecreaseSpeed;
-            fuelText.text = "Fuel: " + (int)fuel;
+            fuelText.text = fuel.ToString("F0");
             if (fuel < 50 && fuel > 15)
-            {
                 fuelText.color = Color.yellow;
-            }
-            if (fuel < 15)
-            {
+
+            else if (fuel < 15)
                 fuelText.color = Color.red;
-            }
-            if (fuel <= 0)
-            {
-                fuelText.text = "Fuel: EMPTY!";
+
+            else if (fuel <= 0) {
+                fuelText.text = "EMPTY!";
                 fuelText.color = Color.red;
                 Destroy(player.gameObject);
             }
         }
-        else
-        {
+        else {
             restartTimer -= Time.deltaTime;
             if (restartTimer <= 0)
                 SceneManager.LoadScene("Game");
         }
 
         //disabled  enemies
-        foreach (Enemy enemy in GetComponentsInChildren<Enemy>())
-        {
-            if(mainCamera.transform.position.y - enemy.transform.position.y > Screen.height / 100f - 1f)
-            {
+        foreach (Enemy enemy in GetComponentsInChildren<Enemy>()) {
+            if(mainCameraTransform.position.y - enemy.transform.position.y > Screen.height / 100f - 1f)
                 enemy.gameObject.SetActive(false);
-            }
         }
     }
 
-    void OnEnemyKill()
-    {
+    void OnEnemyKill() {
         score += 25;
-        scoteText.text = "Score: " + score;
+        scoteText.text = score.ToString();
     }
 
-    void OnCollectFuel()
-    {
+    void OnCollectFuel() {
         fuel = 100f;
         fuelText.color = Color.white;
     }
